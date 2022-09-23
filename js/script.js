4953d87d7de9c4219f39f1d2ba36e500b8ae2831
stylesaniswi <anishshrestha607@gmail.com>
2022-09-23 17:59:42 +0530

    bell added

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function Change(neta) {
+    if (neta == 'oli'){
+        document.getElementById("man").src = "./images/oli.png";
+    } else {
+        document.getElementById("man").src = "http://www.userinterfaceicons.com/80x80/minimize.png";
+    }
+}
